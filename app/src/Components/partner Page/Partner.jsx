37e14a638072ca3e9040da1ed437c0bbd6b8cf2a
aman <aman004dubey@gmail.com>
2022-07-21 13:22:27 +0530

    ok

--- app/src/Components/partner Page/Partner.jsx
+++ app/src/Components/partner Page/Partner.jsx
@@ -18,71 +18,133 @@ export const Partner = () => {
             <div style={{textAlign: 'center'}}><button className='mid-btn'>REQUEST ASSISTANCE FROM BITRIX PARTNERs</button></div>
         </div>
         <div>
-           <img src={imgner}alt="" style={{marginTop:'35px',height:'130px'}} />
+           <img  src={imgner}alt="" style={{marginTop:'35px',height:'130px'}} />
         </div>
         <div className="mid-box">
             <div className="mid-box-div">
-               <p className="mid-box-title">Rajlaxmi Solutions Private Limited</p>
-              <div className="mid-box-gold"> <p className='mid-gold-title' >GOLD</p> <span className='mid-gold-p'>MUMBAI, DELHI, BANGALORE, CHENNAI, HYDERABAD, KOLKATTA, IN</span></div>
-               <p>Established Bitrix Gold Partner, Credible SAP Partner,  Trusted Tally Certified Partner Skill India - Learn - Perform - Earn Your Business success at Rajlaxmi World, Happy to aid you on Sale, Support.</p>
+                <div className="mid-img">
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/authorized_training_provider.png" alt="" />
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/live_chat.png" alt="" />
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/power_seller.png" alt="" />
+                </div>
+               <p className="mid-box-title">BR 24</p>
+              <div className="mid-box-gold"> <p className='mid-gold-title' >GOLD</p> <span className='mid-gold-p'>CHENNAI, HYDERABAD,PUNE,MUMBAI, DELHI, BANGALORE, </span></div>
+               <p  className="mid-box-p">"Br" de Brasil e "24" de Bitrix24. +850 projetos Bitrix24 executados, +1.200 clientes ativos. Geramos valor e resultado para empresas de todos os tamanhos através de serviços e metodologias exclusivas</p>
             </div>
             <div className="mid-box-div">
-               <p className="mid-box-title">All CAD Services Private Limited</p>
+                <div className="mid-img">
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/authorized_training_provider.png" alt="" />
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/live_chat.png" alt="" />
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/power_seller.png" alt="" />
+                </div>
+               <p className="mid-box-title">ASESORES-E</p>
+              <div className="mid-box-gold"> <p className='mid-gold-title'>GOLD</p> <span className='mid-gold-p'> DELHI, BANGALORE, CHENNAI, HYDERABAD, KOLKATTA, IN</span></div>
+               <p  className="mid-box-p" >asesores-e: más de 10 años de experiencia como socios Bitrix, implementaciones atendidas en todos los países hispano hablantes y un objetivo: facilitar la adopción de Bitrix24 en su organización.</p>
+            </div>
+            <div className="mid-box-div">
+                <div className="mid-img">
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/authorized_training_provider.png" alt="" />
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/live_chat.png" alt="" />
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/power_seller.png" alt="" />
+                </div>
+               <p className="mid-box-title">CRM YHINK</p>
+              <div className="mid-box-gold"> <p className='mid-gold-title' >GOLD</p> <span className='mid-gold-p'>INDORE BANGALORE, CHENNAI, HYDERABAD,MUMBAI, DELHI,</span></div>
+               <p  className="mid-box-p">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro eaque ullam et quo? Soluta necessitatibus similique consectetur odit vel eaque quos hic. Praesentium aspernatur soluta molestias ut assumenda dolor voluptatem?</p>
+            </div>
+            <div className="mid-box-div">
+                <div className="mid-img">
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/authorized_training_provider.png" alt="" />
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/live_chat.png" alt="" />
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/power_seller.png" alt="" />
+                </div>
+               <p className="mid-box-title">VITRA NET</p>
               <div className="mid-box-gold"> <p className='mid-gold-title'>GOLD</p> <span className='mid-gold-p'>MUMBAI, DELHI, BANGALORE, CHENNAI, HYDERABAD, KOLKATTA, IN</span></div>
-               <p className="mid-box-p" >Established Bitrix Gold Partner, Credible SAP Partner,  Trusted Tally Certified Partner Skill India - Learn - Perform - Earn Your Business success at Rajlaxmi World, Happy to aid you on Sale, Support.</p>
+               <p  className="mid-box-p" >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro eaque ullam et quo? Soluta necessitatibus similique consectetur odit vel eaque quos hic. Praesentium aspernatur soluta molestias ut assumenda dolor voluptatem?</p>
             </div>
             <div className="mid-box-div">
-               <p className="mid-box-title">Rajlaxmi Solutions Private Limited</p>
+                <div className="mid-img">
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/authorized_training_provider.png" alt="" />
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/live_chat.png" alt="" />
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/power_seller.png" alt="" />
+                </div>
+               <p className="mid-box-title">INTREE-FACE</p>
               <div className="mid-box-gold"> <p className='mid-gold-title' >GOLD</p> <span className='mid-gold-p'>MUMBAI, DELHI, BANGALORE, CHENNAI, HYDERABAD, KOLKATTA, IN</span></div>
-               <p>Established Bitrix Gold Partner, Credible SAP Partner,  Trusted Tally Certified Partner Skill India - Learn - Perform - Earn Your Business success at Rajlaxmi World, Happy to aid you on Sale, Support.</p>
+               <p  className="mid-box-p">sit amet consectetur adipisicing elit. Porro eaque ullam et quo? Soluta necessitatibus similique consectetur odit vel eaque quos hic. Praesentium aspernatur soluta molestias ut assumenda dolor voluptatem?</p>
             </div>
             <div className="mid-box-div">
-               <p className="mid-box-title">All CAD Services Private Limited</p>
+                <div className="mid-img">
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/authorized_training_provider.png" alt="" />
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/live_chat.png" alt="" />
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/power_seller.png" alt="" />
+                </div>
+               <p className="mid-box-title">NEXT TECH CONSULTING</p>
               <div className="mid-box-gold"> <p className='mid-gold-title'>GOLD</p> <span className='mid-gold-p'>MUMBAI, DELHI, BANGALORE, CHENNAI, HYDERABAD, KOLKATTA, IN</span></div>
-               <p className="mid-box-p" >Established Bitrix Gold Partner, Credible SAP Partner,  Trusted Tally Certified Partner Skill India - Learn - Perform - Earn Your Business success at Rajlaxmi World, Happy to aid you on Sale, Support.</p>
+               <p  className="mid-box-p" >Established Bitrix Gold Partner, Credible SAP Partner,  Trusted Tally Certified Partner Skill India - Learn - Perform - Earn Your Business success at Rajlaxmi World, Happy to aid you on Sale, Support.</p>
             </div>
             <div className="mid-box-div">
-               <p className="mid-box-title">Rajlaxmi Solutions Private Limited</p>
+                <div className="mid-img">
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/authorized_training_provider.png" alt="" />
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/live_chat.png" alt="" />
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/power_seller.png" alt="" />
+                </div>
+               <p className="mid-box-title">AKARASOFT</p>
               <div className="mid-box-gold"> <p className='mid-gold-title' >GOLD</p> <span className='mid-gold-p'>MUMBAI, DELHI, BANGALORE, CHENNAI, HYDERABAD, KOLKATTA, IN</span></div>
-               <p>Established Bitrix Gold Partner, Credible SAP Partner,  Trusted Tally Certified Partner Skill India - Learn - Perform - Earn Your Business success at Rajlaxmi World, Happy to aid you on Sale, Support.</p>
+               <p  className="mid-box-p"> Porro eaque ullam et quo? Soluta necessitatibus similique consectetur odit vel eaque quos hic. Praesentium aspernatur soluta molestias ut assumenda dolor voluptatem?</p>
             </div>
             <div className="mid-box-div">
-               <p className="mid-box-title">All CAD Services Private Limited</p>
+                <div className="mid-img">
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/authorized_training_provider.png" alt="" />
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/live_chat.png" alt="" />
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/power_seller.png" alt="" />
+                </div>
+               <p className="mid-box-title">HIRSCHTEC DCS</p>
               <div className="mid-box-gold"> <p className='mid-gold-title'>GOLD</p> <span className='mid-gold-p'>MUMBAI, DELHI, BANGALORE, CHENNAI, HYDERABAD, KOLKATTA, IN</span></div>
-               <p className="mid-box-p" >Established Bitrix Gold Partner, Credible SAP Partner,  Trusted Tally Certified Partner Skill India - Learn - Perform - Earn Your Business success at Rajlaxmi World, Happy to aid you on Sale, Support.</p>
+               <p  className="mid-box-p" >Established Bitrix Gold Partner, Credible SAP Partner,  Trusted Tally Certified Partner Skill India - Learn - Perform - Earn Your Business success at Rajlaxmi World, Happy to aid you on Sale, Support.</p>
             </div>
             <div className="mid-box-div">
-               <p className="mid-box-title">Rajlaxmi Solutions Private Limited</p>
+                <div className="mid-img">
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/authorized_training_provider.png" alt="" />
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/live_chat.png" alt="" />
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/power_seller.png" alt="" />
+                </div>
+               <p className="mid-box-title">ALKEM</p>
               <div className="mid-box-gold"> <p className='mid-gold-title' >GOLD</p> <span className='mid-gold-p'>MUMBAI, DELHI, BANGALORE, CHENNAI, HYDERABAD, KOLKATTA, IN</span></div>
-               <p>Established Bitrix Gold Partner, Credible SAP Partner,  Trusted Tally Certified Partner Skill India - Learn - Perform - Earn Your Business success at Rajlaxmi World, Happy to aid you on Sale, Support.</p>
+               <p  className="mid-box-p">sit amet consectetur adipisicing elit. Porro eaque ullam et quo? Soluta necessitatibus similique consectetur odit vel eaque quos hic. Praesentium aspernatur soluta molestias ut assumenda dolor voluptatem?</p>
             </div>
             <div className="mid-box-div">
-               <p className="mid-box-title">All CAD Services Private Limited</p>
+                <div className="mid-img">
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/authorized_training_provider.png" alt="" />
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/live_chat.png" alt="" />
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/power_seller.png" alt="" />
+                </div>
+               <p className="mid-box-title">LINXYS GmbH</p>
               <div className="mid-box-gold"> <p className='mid-gold-title'>GOLD</p> <span className='mid-gold-p'>MUMBAI, DELHI, BANGALORE, CHENNAI, HYDERABAD, KOLKATTA, IN</span></div>
-               <p className="mid-box-p" >Established Bitrix Gold Partner, Credible SAP Partner,  Trusted Tally Certified Partner Skill India - Learn - Perform - Earn Your Business success at Rajlaxmi World, Happy to aid you on Sale, Support.</p>
+               <p  className="mid-box-p" >sit amet consectetur adipisicing elit. Porro eaque ullam et quo? Soluta necessitatibus similique consectetur odit vel eaque quos hic. Praesentium aspernatur soluta molestias ut assumenda dolor voluptatem?</p>
             </div>
             <div className="mid-box-div">
-               <p className="mid-box-title">Rajlaxmi Solutions Private Limited</p>
+                <div className="mid-img">
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/authorized_training_provider.png" alt="" />
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/live_chat.png" alt="" />
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/power_seller.png" alt="" />
+                </div>
+               <p className="mid-box-title">RADUNZ COMERCIO E SERVIC</p>
               <div className="mid-box-gold"> <p className='mid-gold-title' >GOLD</p> <span className='mid-gold-p'>MUMBAI, DELHI, BANGALORE, CHENNAI, HYDERABAD, KOLKATTA, IN</span></div>
-               <p>Established Bitrix Gold Partner, Credible SAP Partner,  Trusted Tally Certified Partner Skill India - Learn - Perform - Earn Your Business success at Rajlaxmi World, Happy to aid you on Sale, Support.</p>
+               <p  className="mid-box-p">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro eaque ullam et quo? Soluta necessitatibus similique consectetur odit vel eaque quos hic. Praesentium aspernatur soluta molestias ut assumenda dolor voluptatem?</p>
             </div>
             <div className="mid-box-div">
-               <p className="mid-box-title">All CAD Services Private Limited</p>
+                <div className="mid-img">
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/authorized_training_provider.png" alt="" />
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/live_chat.png" alt="" />
+                    <img className="mid-in-img" src="https://www.bitrix24.com/bitrix/components/bx/b24.partner.list/templates/.default/images/power_seller.png" alt="" />
+                </div>
+               <p className="mid-box-title">WEbEE LIMITED</p>
               <div className="mid-box-gold"> <p className='mid-gold-title'>GOLD</p> <span className='mid-gold-p'>MUMBAI, DELHI, BANGALORE, CHENNAI, HYDERABAD, KOLKATTA, IN</span></div>
-               <p className="mid-box-p" >Established Bitrix Gold Partner, Credible SAP Partner,  Trusted Tally Certified Partner Skill India - Learn - Perform - Earn Your Business success at Rajlaxmi World, Happy to aid you on Sale, Support.</p>
-            </div>
-            <div className="mid-box-div">
-               <p className="mid-box-title">Rajlaxmi Solutions Private Limited</p>
-              <div className="mid-box-gold"> <p className='mid-gold-title' >GOLD</p> <span className='mid-gold-p'>MUMBAI, DELHI, BANGALORE, CHENNAI, HYDERABAD, KOLKATTA, IN</span></div>
-               <p>Established Bitrix Gold Partner, Credible SAP Partner,  Trusted Tally Certified Partner Skill India - Learn - Perform - Earn Your Business success at Rajlaxmi World, Happy to aid you on Sale, Support.</p>
-            </div>
-            <div className="mid-box-div">
-               <p className="mid-box-title">All CAD Services Private Limited</p>
-              <div className="mid-box-gold"> <p className='mid-gold-title'>GOLD</p> <span className='mid-gold-p'>MUMBAI, DELHI, BANGALORE, CHENNAI, HYDERABAD, KOLKATTA, IN</span></div>
-               <p className="mid-box-p" >Established Bitrix Gold Partner, Credible SAP Partner,  Trusted Tally Certified Partner Skill India - Learn - Perform - Earn Your Business success at Rajlaxmi World, Happy to aid you on Sale, Support.</p>
+               <p  className="mid-box-p" >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro eaque ullam et quo? Soluta necessitatibus similique consectetur odit vel eaque quos hic. Praesentium aspernatur soluta molestias ut assumenda dolor voluptatem?</p>
             </div>
           
         </div>
     </div>
   )
 }
+
+                                                          
